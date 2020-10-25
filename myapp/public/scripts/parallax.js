215d03d388391db3e3676fd7e8b5ce5f46ec8bf3
Lautaro <barandiaranlautaro@gmail.com>
2020-10-25 15:41:46 -0300

    Efecto parallax, cambio de img (provisoria) de fondo

---
+++ myapp/public/scripts/parallax.js
@@ -0,0 +1,10 @@
+let parallax = document.querySelector('.parallax');
+
+function scrollParallax(){
+    let scrollTop = document.documentElement.scrollTop;
+
+    parallax.style.transform = 'translateY(' + scrollTop * 0.7 + 'px)';
+}
+
+
+window.addEventListener('scroll', scrollParallax);

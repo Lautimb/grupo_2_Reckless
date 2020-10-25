7ee84e40236043bd18d766f1fbcf24e86b110eb8
Lautaro <barandiaranlautaro@gmail.com>
2020-10-25 17:22:37 -0300

    Fixeando menu en el cart

---
+++ myapp/public/scripts/fixed-main.js
@@ -0,0 +1,33 @@
+
+function fixedMain(){
+
+    let scrollTop = document.documentElement.scrollTop;
+    
+    
+    let navBar2 = document.querySelector('#nav-bar2')
+    let mainCart = document.querySelector('#main-cart')
+   
+    console.log(scrollTop);
+     
+   
+        if( scrollTop > 179 ){
+            navBar2.style.position = "fixed";
+            navBar2.style.top = "0px";
+            navBar2.style.backgroundColor = "white";
+            navBar2.style.zIndex = "100";
+            navBar2.style.borderBottom = "0.5px solid #ede6e0";
+            mainCart.style.marginTop= "65px"
+            
+           
+        } else{
+           
+            navBar2.style.borderBottom = "none";
+            navBar2.style.position = "relative";
+            navBar2.style.top = "auto";
+            navBar2.style.backgroundColor = "transparent";
+            mainCart.style.marginTop= "auto"
+        }
+
+}
+
+window.addEventListener('scroll', fixedMain);

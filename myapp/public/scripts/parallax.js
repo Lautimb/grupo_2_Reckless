cfe85cc3380bc1abc916596e38e1ae936e65c7d1
samielee86 <samielee86@gmail.com>
2020-11-12 22:26:03 -0300

    detalles finales para entrega de sprint 3

--- myapp/public/scripts/parallax.js
+++ myapp/public/scripts/parallax.js
@@ -1,9 +1,12 @@
-let parallax = document.querySelector('.parallax');
+const parallax = document.querySelector('.parallax');
+const header = document.querySelector('header');
+
+header.style.position = "absolute";
+header.style.width = "100%";
 
 function scrollParallax(){
     let scrollTop = document.documentElement.scrollTop;
     parallax.style.transform = 'translateY(' + scrollTop * 0.7 + 'px)';
 }
-
 
 window.addEventListener('scroll', scrollParallax);

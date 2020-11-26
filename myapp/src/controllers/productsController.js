765a6d07c5d02c7d621da88d829d48c72ef830c9
Juan Pravata <pravata.juan@gmail.com>
2020-11-26 20:26:34 -0300

    hicimos registro de usuario y carga de producto nuevo con multiples imagenes

--- myapp/src/controllers/productsController.js
+++ myapp/src/controllers/productsController.js
@@ -1,6 +1,30 @@
+const productsHelper = require('../helpers/products-helper');
 const productsController ={
     index: (req,res) =>{
         res.render('products');
+    },
+    create: (req, res) => {
+        res.render('products/upload');
+    },
+    store: (req, res) => {
+        const images = req.files;
+        for(let i = 0; i< images.length; i++){
+            images[i] = images[i].originalname;
+        }
+        
+        const newProduct = {
+            id: productsHelper.generateId(),
+            images: images,
+            name: req.body.name,
+            description: req.body.productDescription,
+            size: req.body.size,
+            type: req.body.type
+        }
+        const allProducts = productsHelper.getAllProducts();
+        const productsToSave = [...allProducts, newProduct];
+        productsHelper.writeProductsData(productsToSave);
+
+        res.redirect('/products/create');
     },
     detail: (req,res) =>{
         res.render('products/detail');

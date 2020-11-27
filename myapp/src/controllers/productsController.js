5ae6656eeb7971bb2be67a24929b21a2d5997498
samielee86 <samielee86@gmail.com>
2020-11-26 22:33:09 -0300

    Product detail dinamico y corregidas las rutas segun consigna de BREAD, falta corregir el nombre del archivo de imgs y terminar funcionalidad de ejs detail

--- myapp/src/controllers/productsController.js
+++ myapp/src/controllers/productsController.js
@@ -1,11 +1,10 @@
-
-const dataBaseHelper = require('../helpers/data-base-helper')
+const dataBaseHelper = require('../helpers/data-base-helper');
 const productsController ={
     index: (req,res) =>{
         res.render('products');
     },
     create: (req, res) => {
-        res.render('products/upload');
+        res.render('products/create');
     },
     store: (req, res) => {
         const images = req.files;
@@ -26,16 +25,31 @@ const productsController ={
         const productsToSave = [...allProducts, newProduct];
         dataBaseHelper.writeNewDataBase(productsToSave,'products-data.json');
 
-        res.redirect('/products/create');
+        res.redirect('products');
     },
     detail: (req,res) =>{
-        res.render('products/detail');
+        /*
+        1. Leer Base de datos completa y buscar el producto entero del id requerido.
+        2. Captar el producto y asignarlo a una variable que debe ser un objeto literal con propiedades y valores.
+        3. Mandar el producto a la vista.
+        */
+        const id = req.params.id;
+        const products = dataBaseHelper.getAllDataBase('products-data.json');
+        const result = products.find((product) => {
+            return product.id == id
+        })
+
+        res.render('products/detail', { product: result });
+    },
+    edit: (req,res) =>{
+        // Renderizar vista de creacion con los campos value del product:id que se quiera editar.
+        res.render('products/create');
     },
     wishlist: (req,res) =>{
         res.render('products/wishlist');
     },
-    upload: (req, res) =>{
-        res.render('products/upload');
+    delete: (req, res) =>{
+        res.render('products/detail');
     }
     
 
--- myapp/src/helpers/data-base-helper.js
+++ myapp/src/helpers/data-base-helper.js
@@ -2,14 +2,20 @@ const path = require('path');
 const fs = require('fs');
 
 const dataBaseHelper = {
-    getAllDataBase: (dataBaseFile) =>{
+
+    // Encuentra el archivo y lee la base de datos requerida.
+    getAllDataBase: (dataBaseFile) =>{ 
         const dataBasePath = path.join('./src/data', dataBaseFile);
         return JSON.parse(fs.readFileSync(dataBasePath, 'utf-8'));
     },
+
+    // Generar nuevo id a continuacion del ultimo id de la lista en la base de datos.
     generateId: (dataBaseFile) =>{
         const allDataList = dataBaseHelper.getAllDataBase(dataBaseFile);
         return allDataList.pop().id +1;
     },
+
+    // Escribir el nuevo dato e incluirlo al final de la lista en la base de datos.
     writeNewDataBase: (dataToSave, dataBaseFile) => {
         const dataToStringify = JSON.stringify(dataToSave, null, ' ');
         const dataBasePath = path.join('./src/data', dataBaseFile);

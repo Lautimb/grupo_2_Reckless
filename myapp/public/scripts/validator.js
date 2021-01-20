bc996305a26d796d9f06cb48c2c6c91a5aead8a9
Juan Pravata <pravata.juan@gmail.com>
2021-01-20 19:51:47 -0300

    inicio validaciones de front

---
+++ myapp/public/scripts/validator.js
@@ -0,0 +1,17 @@
+const register = document.querySelector("#register-form");
+const login = document.querySelector(".form-login-style");
+const product = document.querySelector("#product-upload__form")
+
+register.addEventListener("submit", (event)=>{
+    event.preventDefault()
+})
+
+
+login.addEventListener("submit", (event)=>{
+    event.preventDefault()
+})
+
+
+product.addEventListener("submit", (event)=>{
+     event.preventDefault()
+})

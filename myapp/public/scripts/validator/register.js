d045cc346060257e1761658d95a1342b9088f651
blackandwhite <barandiaranlautaro@gmail.com>
2021-01-27 19:29:34 -0300

    comienzo de api

--- myapp/public/scripts/validator/register.js
+++ myapp/public/scripts/validator/register.js
@@ -3,7 +3,7 @@ const registerButton = document.querySelector("#button-register");
 const errorElement = document.querySelector(".errors");
 
 
-registerButton.addEventListener("click", (event)=>{
+registerButton.addEventListener("click", (_event)=>{
     const errors = []
     errorElement.innerHTML = ""
     const name = document.querySelector("#firstName");
@@ -15,6 +15,24 @@ registerButton.addEventListener("click", (event)=>{
     const month = document.querySelector("#date-mm");
     const year = document.querySelector("#date-yyyy");
     const passwordRegex =  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    
+
+    // NO SE COMO ACCEDER A LA INFO QUE TRAIGO, NO LA PUEDO SACAR DEL CALLBACK
+
+    fetch('http://localhost:3000/api/users',{
+        method: 'POST',
+        body: JSON.stringify(),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((res)=> res.json())
+            .then(function(users){
+                console.log(users)
+      
+            })
+         
+   
 
     if(name.value.trim().length < 2){
         errors.push("Your name must contain at least 2 characters.");
@@ -26,7 +44,7 @@ registerButton.addEventListener("click", (event)=>{
 
     if(email.value.length == 0){
         errors.push("Your email is required.");
-    }
+    }    
 
     if(!password.value.match(passwordRegex)){
         errors.push("Your password is required and must contain at least 8 characters and must contain an upper case character and one o more numbers");

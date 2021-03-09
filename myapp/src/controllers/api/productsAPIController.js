58149d3754ed4ab3814c04b303675cc0c3c6a3ae
samielee86 <samielee86@gmail.com>
2021-03-08 22:07:45 -0300

    comiteando por las dudas

--- myapp/src/controllers/api/productsAPIController.js
+++ myapp/src/controllers/api/productsAPIController.js
@@ -15,10 +15,10 @@ module.exports = {
                     nested: true
                 }],
                 order: ["id"],
-                limit: 4,
-                offset: 4 * (page - 1),
+                limit: 2,
+                offset: 2 * (page - 1),
             })
-            const totalPages = Math.ceil(paginatedProducts.count / 4)
+            const totalPages = Math.ceil(paginatedProducts.count / 2)
             
             // SETTING IMGS
             paginatedProducts.rows.forEach( product => {
@@ -59,7 +59,6 @@ module.exports = {
             })
 
             const typeObject = Object.fromEntries(type);
-            
 
             res.json({
                 meta: {
@@ -75,7 +74,7 @@ module.exports = {
                     nextPage:  `http://localhost:3300/api/products?page=${page + 1}`
                 },
                 paginatedProducts
-               
+                
             })
         } catch (error) {
             res.status(500).json({

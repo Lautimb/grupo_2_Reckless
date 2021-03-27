98b1341a32d8cd0402afc2532fd80cb348d03436
samie <samielee86@gmail.com>
2021-03-27 16:05:37 -0300

    add more button funciona. falta definir save o cancel. product/create

--- myapp/public/scripts/productStock.js
+++ myapp/public/scripts/productStock.js
@@ -1,7 +1,11 @@
 const addMoreButton = document.querySelector('#add-more-button')
+const clearButton = document.querySelector('.clear-button')
+const sizeColorContainer = document.querySelector("#div-upload-data")
 
 addMoreButton.onclick = (e) => {
-    const sizeColorForm = document.querySelector('.ul-upload-data')
-    const cln = sizeColorForm.cloneNode(true)
-    document.querySelector('#div-upload-data').appendChild(cln);
+    const newSizeColor = sizeColorContainer.lastElementChild
+    const newCln = newSizeColor.cloneNode(true)
+    let i = newCln.getAttribute("id")
+    newCln.setAttribute("id", ++i)
+    sizeColorContainer.appendChild(newCln);
 }
